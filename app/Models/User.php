13322132570795e4233f699b3ba61194e0523b25
dafialmadinah <dafi.almadinah0511@gmail.com>
2026-04-19 15:04:32 +0700

    feat(auth): implement JWTSubject on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -30,5 +31,15 @@ class User extends Authenticatable
     public function joinRequests()
     {
         return $this->hasMany(JoinRequest::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
