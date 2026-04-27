3dcb7fbb6ebccb7bdeb3682c51a908988a0b2e69
aditri24sn <aditrisurya@gmail.com>
2026-04-27 12:14:38 +0700

    feat: add join request model and migration model

---
+++ app/Models/JoinRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JoinRequest extends Model
+{
+    use HasFactory;
+
+    protected $table = 'join_requests';
+
+    protected $fillable = [
+        'user_id',
+        'idea_id',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function idea()
+    {
+        return $this->belongsTo(Idea::class);
+    }
+}
