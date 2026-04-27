cab19d379dc0959fb22c834fb644f39bdeb6674a
aditri24sn <aditrisurya@gmail.com>
2026-04-27 12:17:22 +0700

    feat : update route for Join Request

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\IdeaController;
-
+use App\Http\Controllers\Api\JoinRequestController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,5 +11,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::apiResource('ideas', IdeaController::class);
-
+    Route::apiResource('requests', JoinRequestController::class);
 });
